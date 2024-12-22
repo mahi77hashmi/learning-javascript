5857fb87dd5c35f1b905ed72de6558c26dd6127e
mahi77hashmi <160375918+mahi77hashmi@users.noreply.github.com>
2024-12-22 06:15:19 +0000

    about variable lesson completed

---
+++ day_02/main.js
@@ -0,0 +1,10 @@
+const accountId = 79781
+let accountEmail = "mahi.hashmi@zindex.cloud"
+var accountPasscode = "learningJavascript"
+accountCity = "Naogaon"
+let accountState;
+
+//accountId = 27203 (This is not allowed with const variable)
+/*don't ever use var in your code */
+
+console.table([accountId, accountEmail, accountPasscode, accountCity, accountState])
