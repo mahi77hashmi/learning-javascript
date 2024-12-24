2d1bfc6a21bca3c2dbefd671a277df7017b81c14
Mahi Hashmi <160375918+mahi77hashmi@users.noreply.github.com>
2024-12-24 06:46:05 +0000

    Conversion Operation JS

---
+++ day_04/conversionOperation.js
@@ -0,0 +1,34 @@
+// Suppose you have a form in the footer of the website, and You want to extract the data your user gave you through the form,
+//  how can you get the data and save it in your server?
+// We will need data conversion method
+
+let userName = "Mahi Hashmi"
+let email = "mahi.hashmi@zindex.cloud"
+let isSubscribed = 0
+let cellNumber = 88012345678
+let uniqueId = "776554"
+
+let stringUsername = String(userName)
+let stringEmail = String(email)
+let booleanSubscribe = Boolean(isSubscribed)
+let stringCellNumber = String(cellNumber)
+let numberUniqueId = Number(uniqueId)
+
+console.log(stringUsername)
+console.log(stringEmail)
+console.log(stringCellNumber)
+console.log(booleanSubscribe);
+console.log(numberUniqueId);
+console.log(typeof stringUsername)
+console.log(typeof stringEmail)
+console.log(typeof stringCellNumber)
+console.log(typeof booleanSubscribe);
+console.log(typeof numberUniqueId); 
+
+//some inverstigations
+
+// let myAge = 19;
+// let booleanMyAge = Boolean(myAge)
+// console.log(booleanMyAge) //true 
+// console.log(typeof booleanMyAge) //boolean
+
